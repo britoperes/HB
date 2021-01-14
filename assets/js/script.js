8830424d9de08cf0de3ded31995b7c84d13b55bd
João Pedro Peres <britoperes@icloud.com>
2021-01-14 17:03:35 -0300

    Rodape finalizado

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,7 @@
-let inputNome = document.getElementById('inputNome');
-let inputEmail = document.getElementById('inputEmail');
-let inputProcedimento = document.getElementById('inputProcedimento');
-let btnForm = document.getElementById('btnform');
+const msgDireitos = document.getElementById('rodape')
 
-const test = ()=>{
-    if(inputNome.value===''&&inputEmail.value===''&&inputProcedimento.value==='Selecione um procedimento')
-    {return}
-    else{
-        console.log(inputNome.value);
-        console.log(inputEmail.value);
-        console.log(inputProcedimento.value);
-    }
-    
-}
+let anoAtual = new Date();
 
-$('.nav a').on('click', function(){
-    $('.navbar-toggler').click(); //bootstrap 4.x
-});
+let msgAtualizada = `HB Clinica Estética ${anoAtual.getFullYear()}. Todos os direitos reservados. CNPJ 39.910.077/0001-34`;
 
+msgDireitos.textContent = msgAtualizada;
